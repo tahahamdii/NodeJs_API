499b07b43edd73dd1b2be9f97ba032b01a455464
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-16 22:08:02 +0100

    post crud working

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
  const express = require ('express')
  const app = express()
+ const Product = require('./models/productModel')
 const mongoose = require ('mongoose')
+
+app.use(express.json())
  //routes 
  app.get('/',(req,res,next) => {
     res.send('Hello taha')
@@ -9,6 +12,15 @@ const mongoose = require ('mongoose')
     res.send('Hello blog')
  })
 
+ app.post('/product', async(req,res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error){
+        console.log(error.message);
+        res.status(500).json({message:error.message})
+    }
+ })
 
  mongoose.set("strictQuery", false)
  mongoose.
