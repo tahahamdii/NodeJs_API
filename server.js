94eba058b8466f63a1a50cdcc1ea2144f03acf98
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-16 22:28:25 +0100

    delete crud

--- server.js
+++ server.js
@@ -60,6 +60,20 @@ app.use(express.json())
     }
  })
 
+ app.delete('/products/:id', async(req,res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find id ${id}`})
+        }
+        res.status(200).json()
+    }catch (error){
+        console.log(error.message);
+        res.status(500).json({message:error.message})
+    }
+ })
+
  mongoose.set("strictQuery", false)
  mongoose.
  connect('mongodb+srv://admin:Admin12345@cluster0.c8p6jiq.mongodb.net/Node-API?retryWrites=true&w=majority')
