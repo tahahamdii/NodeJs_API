90d2f2cea6fa1138e76d623f48ad587234d26e72
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-16 21:51:38 +0100

    Shcemas

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
  const express = require ('express')
- const app = express('mongoose')
-const mongoose = require 
+ const app = express()
+const mongoose = require ('mongoose')
  //routes 
  app.get('/',(req,res,next) => {
     res.send('Hello taha')
@@ -9,13 +9,14 @@ const mongoose = require
     res.send('Hello blog')
  })
 
- app.listen(3000, () => {
-    console.log('node api running')
- })
 
+ mongoose.set("strictQuery", false)
  mongoose.
  connect('mongodb+srv://admin:Admin12345@cluster0.c8p6jiq.mongodb.net/Node-API?retryWrites=true&w=majority')
  .then(() => {
+    app.listen(3000, () => {
+        console.log('node api running')
+     })
     console.log('connected to db!!')
  }).catch((error) => {
     console.log(error)
