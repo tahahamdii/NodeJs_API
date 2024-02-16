0be21435c33d14d83acbf76c649bbfeb1ace82af
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-16 21:40:57 +0100

    connecting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
  const express = require ('express')
- const app = express()
-
+ const app = express('mongoose')
+const mongoose = require 
  //routes 
  app.get('/',(req,res,next) => {
     res.send('Hello taha')
@@ -11,4 +11,12 @@
 
  app.listen(3000, () => {
     console.log('node api running')
+ })
+
+ mongoose.
+ connect('mongodb+srv://admin:Admin12345@cluster0.c8p6jiq.mongodb.net/Node-API?retryWrites=true&w=majority')
+ .then(() => {
+    console.log('connected to db!!')
+ }).catch((error) => {
+    console.log(error)
  })
